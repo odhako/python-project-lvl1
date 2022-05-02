d6be04a6d21c0202bffb5d84e067f61c5b335de2
odhako <odhako@gmail.com>
2022-05-02 23:41:05 +0400

    Rename game_interface to game_start

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 
-from brain_games.game_interface import game_interface
+from brain_games.game_start import game_start
 from brain_games.games.gcd import round_generator, get_rules
 
 
 def main():
-    game_interface(get_rules, round_generator)
+    game_start(get_rules, round_generator)
 
 
 if __name__ == '__main__':
